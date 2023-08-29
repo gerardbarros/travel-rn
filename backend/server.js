7e63eb39284f4370e6bf245008e63de942feb27d
Gerard <barros.gc@gmail.com>
2023-08-29 00:35:02 -0500

    Create error middleware and add to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 const port = process.env.PORT || 6000
 import userRoutes from './routes/userRoutes.js' 
 
 const app = express()
 
 app.use('/api/users', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 app.get('/', (req, res) => res.send('Server is running'))
 
